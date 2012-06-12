9c9edac827162ebaa0e131c33e0d5ff5c68e1355
juliaelman <julia.elman@gmail.com>
2012-06-12 02:47:53 -0400

    Adding .DS_Store to .gitignore and an example local_settings.py

---
+++ letsgetlouder/local_settings.example.py
@@ -0,0 +1,9 @@
+from letsgetlouder.settings import *
+
+# You will need to get these from either Paul or Julia
+TWITTER_CONSUMER_KEY = ''                              
+TWITTER_CONSUMER_SECRET = ''          
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''                    
+GITHUB_APP_ID = ''                                
+GITHUB_API_SECRET = ''
