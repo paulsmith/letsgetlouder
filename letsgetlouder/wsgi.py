f2d2425225f10c0292625131baca560f03e7cc06
Colin Copeland <copelco@caktusgroup.com>
2012-06-16 17:56:11 -0400

    again :)

--- letsgetlouder/wsgi.py
+++ letsgetlouder/wsgi.py
@@ -17,7 +17,8 @@ import os
 import site
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-path = os.path.abspath(os.path.join(ROOT, os.pardir))
+ROOT = os.path.abspath(os.path.join(ROOT, os.pardir))
+path = os.path.join(ROOT, 'env', 'lib', 'python2.6', 'site-packages')
 site.addsitedir(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "letsgetlouder.settings")
