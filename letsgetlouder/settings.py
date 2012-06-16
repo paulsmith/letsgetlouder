66d09fc978bdf86d15d3a93346ac729de18e4bae
Colin Copeland <copelco@caktusgroup.com>
2012-06-16 17:20:18 -0400

    add static/media root

--- letsgetlouder/settings.py
+++ letsgetlouder/settings.py
@@ -3,6 +3,7 @@ import os.path
 import sys
 
 where = os.path.dirname(__file__)
+root = os.path.abspath(os.path.join(where, os.pardir))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -53,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(root, 'media_root')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(root, 'static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
