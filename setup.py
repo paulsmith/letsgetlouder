854cf82e95f842c42b55ece5a25c7c4d536c1239
juliaelman <julia.elman@gmail.com>
2012-06-17 16:19:53 -0400

    Adding more requirements.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='Django community pledge site',
     url='http://letsgetlouder.com/',
-    install_requires=['Django==1.4', 'django-social-auth==0.6.9', 'django-compressor==1.1.1', 'South==0.7.4', 'psycopg2==2.4.5'],
+    install_requires=['Django==1.4', 'django-social-auth==0.6.9', 'django-compressor==1.1.2', 'South==0.7.4', 'psycopg2==2.4.5', 'python-memcached==1.47', 'BeautifulSoup==3.2.0'],
     packages=find_packages(),
     license='BSD'
 )
