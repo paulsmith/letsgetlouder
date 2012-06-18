d58004c5ac72cc6cbb9307e4fbb555ae700e2196
juliaelman <julia.elman@gmail.com>
2012-06-17 21:57:13 -0400

    Adding django-debug-toolbar to optional dev apps.

--- letsgetlouder/local_settings.example.py
+++ letsgetlouder/local_settings.example.py
@@ -6,6 +6,16 @@ COMPRESS_ENABLED = False
 
 INTERNAL_IPS = ('127.0.0.1', )
 
+# Optional settings if you want to use Django debug toolbar
+
+# MIDDLEWARE_CLASSES += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+
+# INSTALLED_APPS += (
+#     'debug_toolbar',  
+# )
+
 # You will need to get these from either Paul or Julia
 TWITTER_CONSUMER_KEY = ''                              
 TWITTER_CONSUMER_SECRET = ''          
