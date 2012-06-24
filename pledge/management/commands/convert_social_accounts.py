73b6790c1de0da3d6585402a69f47c3b4dd201f2
mlavin <markdlavin@gmail.com>
2012-06-24 15:45:30 -0400

    Management command to convert existing django-social-auth users to django-all-access.

---
+++ pledge/management/commands/convert_social_accounts.py
@@ -0,0 +1,40 @@
+from django.core.management.base import NoArgsCommand, CommandError
+
+from allaccess.models import Provider, AccountAccess 
+
+
+class Command(NoArgsCommand):
+    "Convert existing signatures from django-social-auth to django-all-access."
+
+    def handle_noargs(self, **options):
+        try:
+            from social_auth.models import UserSocialAuth
+        except ImportError:
+            raise CommandError("django-social-auth is not installed.")
+        # Create providers if they don't already exist
+        defaults = {
+            'authorization_url': 'https://www.facebook.com/dialog/oauth',
+            'access_token_url': 'https://graph.facebook.com/oauth/access_token',
+            'profile_url': 'https://graph.facebook.com/me',
+        }
+        facebook, _ = Provider.objects.get_or_create(name='facebook', defaults=defaults)
+        defaults = {
+            'request_token_url': 'https://api.twitter.com/oauth/request_token',
+            'authorization_url': 'https://api.twitter.com/oauth/authenticate',
+            'access_token_url': 'https://api.twitter.com/oauth/access_token',
+            'profile_url': 'https://twitter.com/account/verify_credentials.json',
+        }
+        twitter, _ = Provider.objects.get_or_create(name='twitter', defaults=defaults)
+        for social in UserSocialAuth.objects.all():
+            provider = None
+            if social.provider == 'facebook':
+                provider = facebook
+            elif social.provider == 'twitter':
+                provider = twitter
+            if provider is not None:
+                defaults = {
+                    'user': social.user,
+                }
+                access, _ = AccountAccess.objects.get_or_create(
+                    identifier=social.uid, provider=provider, defaults=defaults
+                )
