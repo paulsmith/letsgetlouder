f5f1077d252700bdbc70d988319b9ae87b1ea573
mlavin <markdlavin@gmail.com>
2012-06-24 22:06:58 -0400

    Name the account url.

--- letsgetlouder/urls.py
+++ letsgetlouder/urls.py
@@ -9,7 +9,7 @@ from .views import LoginRedirect, LoginCallback
 
 urlpatterns = patterns('',
     url(r'^$', 'letsgetlouder.views.index_view'),
-    url(r'^account/$', TemplateView.as_view(template_name='account.html')),
+    url(r'^account/$', TemplateView.as_view(template_name='account.html'), name="account"),
     url(r'^sign/$', 'pledge.views.sign_view', name='sign-pledge'),
     url(r'^unsign/$', 'pledge.views.unsign_view', name='unsign-pledge'),
     url(r'^log-out/$', 'letsgetlouder.views.logout_view'),
